8d19ab309f82bd61c87c27e99ac9a0445fe73d28
Денис <sarm.dn@yandex.ru>
2023-11-20 21:49:14 +0300

    _padding

--- js/main.js
+++ js/main.js
@@ -10,30 +10,25 @@ $(document).ready(function() {
 // спойлер
 
 
-
 $(document).ready(function() {
     $('.question__box-button').click(function(event) {
-        // Удаляем класс active у всех кнопок
-        $('.question__box-button').removeClass('active');
-
-        // Добавляем/удаляем класс active для текущей кнопки
-        $(this).toggleClass('active');
-
-        // Скрываем все блоки текста
-        $('.question__box-text').slideUp(300);
-
-        // Показываем блок текста для текущей кнопки, если она имеет класс active
+        // Проверяем, если текущая кнопка уже имеет класс active
         if ($(this).hasClass('active')) {
+            // Если да, то удаляем класс active и скрываем блок текста
+            $(this).removeClass('active');
+            $(this).closest('.question__box-title').next('.question__box-text').slideUp(300);
+        } else {
+            // Иначе, удаляем класс active у всех кнопок
+            $('.question__box-button').removeClass('active');
+            // Добавляем класс active для текущей кнопки
+            $(this).addClass('active');
+            // Скрываем все блоки текста
+            $('.question__box-text').slideUp(300);
+            // Показываем блок текста для текущей кнопки
             $(this).closest('.question__box-title').next('.question__box-text').slideDown(300);
         }
     });
 });
-
-// $(document).ready(function() {
-//     $('.question__box-button').click(function(event) {
-//         $(this).toggleClass('active').closest('.question__box-title').next('.question__box-text').slideToggle(300);
-//     });
-// });
 
 //анимация появления 
 
@@ -54,4 +49,35 @@ document.addEventListener("scroll", function() {
     handleScrollAnimation(elementsFadeInR, "show-x");
     handleScrollAnimation(elementsFadeInL, "show-x");
 });
+
+
+//спойлер не закрывается
+
+// $(document).ready(function() {
+//     $('.question__box-button').click(function(event) {
+//         // Удаляем класс active у всех кнопок
+//         $('.question__box-button').removeClass('active');
+
+//         // Добавляем/удаляем класс active для текущей кнопки
+//         $(this).toggleClass('active');
+
+//         // Скрываем все блоки текста
+//         $('.question__box-text').slideUp(300);
+
+//         // Показываем блок текста для текущей кнопки, если она имеет класс active
+//         if ($(this).hasClass('active')) {
+//             $(this).closest('.question__box-title').next('.question__box-text').slideDown(300);
+//         }
+//     });
+// });
+
+//спойлер открыты все
+
+// $(document).ready(function() {
+//     $('.question__box-button').click(function(event) {
+//         $(this).toggleClass('active').closest('.question__box-title').next('.question__box-text').slideToggle(300);
+//     });
+// });
+
+
 
